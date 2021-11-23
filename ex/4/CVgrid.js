57e32e16c8d671cfab325d51c38982c31d82cf0e
OmerWaizman <omezn289@gmail.com>
2021-11-23 20:36:04 +0200

    5

---
+++ ex/4/CVgrid.js
@@ -0,0 +1,10 @@
+function OmerFunction(){
+
+    var user={
+        firstName='Omer',
+        lastName='Waizman',
+        email:'omezn289@gmail.com',
+        age:27
+    }
+    console.log(user);
+}
